1cf0ff2bd5452719ccd05b4969ceb690b46077fd
Dilate-byte <dikshaghimire565@gmail.com>
2024-04-03 21:38:16 +0545

    backend added

---
+++ backend/Ecommerce-ejb/target/generated-sources/annotations/com/entities/User_.java
@@ -0,0 +1,21 @@
+package com.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-04-03T21:33:30")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, String> password;
+    public static volatile SingularAttribute<User, String> address;
+    public static volatile SingularAttribute<User, String> name;
+    public static volatile SingularAttribute<User, String> contactNumber;
+    public static volatile SingularAttribute<User, Boolean> active;
+    public static volatile SingularAttribute<User, Long> id;
+    public static volatile SingularAttribute<User, String> userType;
+    public static volatile SingularAttribute<User, String> email;
+    public static volatile SingularAttribute<User, Boolean> status;
+
+}
